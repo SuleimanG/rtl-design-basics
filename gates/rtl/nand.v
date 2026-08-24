c7845a134bf65d39d094d25a387e230855387980
SuleimanG <mohamedsoliman9091@gmail.com>
2026-08-24 19:03:57 +0300

    Delete gates directory

--- gates/rtl/nand.v
+++
@@ -1,8 +0,0 @@
-
-module nand_gate (
-    input wire a,
-    input wire b,
-    output wire y
-);
-    assign y = ~(a & b);
-endmodule
--- gates/sim/run.sh
+++
@@ -1,3 +0,0 @@
-iverilog -o sim.vvp ../rtl/nand.v ../tb/nand_tb.v
-vvp sim.vvp
-gtkwave nand.vcd
